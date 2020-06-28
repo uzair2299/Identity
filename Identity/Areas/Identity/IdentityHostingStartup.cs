cb3fb7cd0bc3e30fed46bed64adbbf973953fa9e
uzair2299 <uzairanwar2299@gmail.com>
2020-06-28 17:13:44 +0500

    Add project files.

---
+++ Identity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Identity.Areas.Identity.IdentityHostingStartup))]
+namespace Identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+            });
+        }
+    }
+}
---
+++ Identity/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Models
+{
+    public class ApplicationUser :IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string fullname { get; set; }
+
+        public string halfName { get; set; }
+    }
+}
